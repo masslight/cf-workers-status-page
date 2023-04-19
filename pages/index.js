bb5eb83a538d40fb1b8f3f7b266cca80f35f9b08
saewitz <jonathansaewitz@gmail.com>
2023-04-19 10:32:11 -0400

    update text and logo and run every other minute

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
         <div className="flex flex-row justify-between items-center p-4">
           <div className="flex flex-row items-center">
             <img className="h-8 w-auto" src={config.settings.logo} />
-            <h1 className="ml-4 text-3xl">{config.settings.title}</h1>
+            <h1 className="ml-4 text-3xl">{config.settings.header}</h1>
           </div>
           <div className="flex flex-row items-center">
             {typeof window !== 'undefined' && <ThemeSwitcher />}
